05f8f3eabc82c14af705d9b1ee895b0511d2227b
idbentley <ian.bentley@gmail.com>
2014-07-11 10:29:25 -0400

    reorg repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+requires = []
+
+setup(
+    name="skunkqueue",
+    version="0.0.1",
+    packages=["skunkqueue"],
+    description="Asynchronous task runner with MongoDB backend",
+)
